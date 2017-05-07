00ea29b4563b16db9255b947198461e49e439ae0
Muhammad Hazim <mhazim2@gmail.com>
2017-05-07 14:54:43 +0700

    update 1

--- app/src/main/java/com/impact/mhazim/animalium/Main2Activity.java
+++ app/src/main/java/com/impact/mhazim/animalium/Main2Activity.java
@@ -1,13 +1,34 @@
 package com.impact.mhazim.animalium;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    EditText mNama;
+    Button mMasuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        mNama = (EditText) findViewById(R.id.mEditTextNama);
+        mMasuk = (Button) findViewById(R.id.btnMasuk);
+
+        mMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j;
+                j = new Intent(Main2Activity.this, MainActivity.class);
+                j.putExtra("NAMA", mNama.getText().toString());
+                startActivity(j);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/impact/mhazim/animalium/MainActivity.java
+++ app/src/main/java/com/impact/mhazim/animalium/MainActivity.java
@@ -2,12 +2,21 @@ package com.impact.mhazim.animalium;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNama = (TextView) findViewById(R.id.mTextNama);
+
+        String namadarifront = getIntent().getStringExtra("NAMA");
+
+        mNama.setText(namadarifront);
     }
 }
